a7c93c89cbe3bad6c5cfd4418986bf21a4010344
shimopino <shimoke4869@gmail.com>
2022-05-30 04:02:07 +0900

    Red: パスワードの再入力の検証を追加

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,4 +1,26 @@
+import React, { useState } from "react";
+
 export const SignUpPage = () => {
+  const [disabled, setDisabled] = useState(true);
+  const [password, setPassword] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const currentInput = e.currentTarget.value;
+    setPassword(currentInput);
+    if (currentInput && passwordRepeat)
+      setDisabled(currentInput !== passwordRepeat);
+  };
+
+  const onChangePasswordRepeat = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("called");
+    const currentInput = e.currentTarget.value;
+    setPasswordRepeat(currentInput);
+    if (password && currentInput) setDisabled(password !== currentInput);
+  };
+
+  console.log({ password, passwordRepeat, disabled });
+
   return (
     <>
       <h1>Sign Up</h1>
@@ -7,10 +29,20 @@ export const SignUpPage = () => {
       <label htmlFor="email">Email</label>
       <input id="email" type="text" />
       <label htmlFor="password">Password</label>
-      <input id="password" type="password" />
+      <input
+        id="password"
+        type="password"
+        value={password}
+        onChange={onChangePassword}
+      />
       <label htmlFor="passwordRepeat">Password Repeat</label>
-      <input id="passwordRepeat" type="password" />
-      <button disabled>Sign Up</button>
+      <input
+        id="passwordRepeat"
+        type="password"
+        value={passwordRepeat}
+        onChange={onChangePasswordRepeat}
+      />
+      <button disabled={disabled}>Sign Up</button>
     </>
   );
 };
