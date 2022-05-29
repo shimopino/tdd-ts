643eb2307d1d5fc4c2a1fe157bc44c6d8dce5f70
shimopino <shimoke4869@gmail.com>
2022-05-29 09:43:06 +0900

    サーバー起動時にデータベースを初期化する

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import { app } from "./app";
+import { sequelize } from "./config/database";
 
 const port = process.env.PORT ? process.env.PORT : 3010;
+
+sequelize.sync();
 
 app.listen(port, () => {
   console.log("hi dev");
