34e27e0b2286609b03db4d1402af02cce014376c
shimopino <shimoke4869@gmail.com>
2022-05-29 10:53:26 +0900

    node-config と cross-env でコマンドごとに動的に設定ファイルを適用する

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,9 +1,26 @@
-import { Sequelize } from "sequelize";
+import { Dialect, Sequelize } from "sequelize";
+import config from "config";
 
-const sequelize = new Sequelize("hoaxify", "my-db-user", "db-p4ss", {
-  dialect: "sqlite",
-  storage: "./database.sqlite",
-  logging: false,
-});
+type DB_CONFIG = {
+  database: string;
+  username: string;
+  password: string;
+  dialect: Dialect;
+  storage: string;
+  logging: boolean;
+};
+
+const dbConfig = config.get<DB_CONFIG>("database");
+
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    dialect: dbConfig.dialect,
+    storage: dbConfig.storage,
+    logging: dbConfig.logging,
+  }
+);
 
 export { sequelize };
