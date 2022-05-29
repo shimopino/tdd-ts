d0177b1df327da5333c06bc03099e71f1e4dfe0d
shimopino <shimoke4869@gmail.com>
2022-05-29 11:58:09 +0900

    Refactoring: レイアウトと機能を別々のテストスイートとしてまとめる

--- frontend/src/pages/SignUpPage.spec.tsx
+++ frontend/src/pages/SignUpPage.spec.tsx
@@ -1,9 +1,13 @@
 import { render, screen } from "@testing-library/react";
-import { it } from "vitest";
+import { describe, it } from "vitest";
 import { SignUpPage } from "./SignUpPage";
 
-it("has header", () => {
-  render(<SignUpPage />);
-  const header = screen.getByRole("heading", { name: "Sign Up" });
-  expect(header).toBeInTheDocument();
+describe("Sign Up Page", () => {
+  describe("Layout", () => {
+    it("has header", () => {
+      render(<SignUpPage />);
+      const header = screen.getByRole("heading", { name: "Sign Up" });
+      expect(header).toBeInTheDocument();
+    });
+  });
 });
