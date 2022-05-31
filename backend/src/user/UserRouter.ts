eed8d05c2d4ec26f7b215583fb1bc648da759992
shimopino <shimoke4869@gmail.com>
2022-05-31 23:17:21 +0900

    Green: リクエストボディに対する検証処理を追加

--- backend/src/user/UserRouter.ts
+++ backend/src/user/UserRouter.ts
@@ -12,10 +12,25 @@ router.post(
     .notEmpty()
     .withMessage("Username cannot be null")
     .bail()
-    .isLength({ min: 4 })
+    .isLength({ min: 4, max: 32 })
     .withMessage("Must have min 4 and max 32 characters"),
-  check("email").notEmpty().withMessage("Email cannot be null"),
-  check("password").notEmpty().withMessage("Password cannot be null"),
+  check("email")
+    .notEmpty()
+    .withMessage("Email cannot be null")
+    .bail()
+    .isEmail()
+    .withMessage("Email is not valid"),
+  check("password")
+    .notEmpty()
+    .withMessage("Password cannot be null")
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters")
+    .bail()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
+    .withMessage(
+      "Password must be at least 1 uppercase, 1 lowercase letter and 1 number"
+    ),
   async (req: CustomRequest<CreateUserDTO>, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
