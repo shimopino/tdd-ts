3b762baad196380e845236aa0a565e2b2f5436fc
shimopino <shimoke4869@gmail.com>
2022-05-31 23:01:09 +0900

    Green: 文字列長の検証処理とエラーが発生した段階でストップする bail を追加

--- backend/src/user/UserRouter.ts
+++ backend/src/user/UserRouter.ts
@@ -8,7 +8,12 @@ const router = express.Router();
 
 router.post(
   "/api/1.0/users",
-  check("username").notEmpty().withMessage("Username cannot be null"),
+  check("username")
+    .notEmpty()
+    .withMessage("Username cannot be null")
+    .bail()
+    .isLength({ min: 4 })
+    .withMessage("Must have min 4 and max 32 characters"),
   check("email").notEmpty().withMessage("Email cannot be null"),
   check("password").notEmpty().withMessage("Password cannot be null"),
   async (req: CustomRequest<CreateUserDTO>, res) => {
