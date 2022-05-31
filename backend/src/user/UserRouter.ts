e1d4ca911b611de84ef609f4e42cfd7a1ef05cf4
shimopino <shimoke4869@gmail.com>
2022-05-31 22:41:54 +0900

    Refactoring: express-validatorを使用した検証処理のリファクタリングを追加

--- backend/src/user/UserRouter.ts
+++ backend/src/user/UserRouter.ts
@@ -1,43 +1,22 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { CustomRequest, CreateUserDTO } from "./dtos";
+import { check, validationResult } from "express-validator";
 
 import { save } from "./UserService";
 
 const router = express.Router();
 
-const validateUsername = (req: Request, res: Response, next: NextFunction) => {
-  const user = { ...req.body };
-  if (user.username === null) {
-    // @ts-expect-error 一旦型付けを無視する
-    req.validationErrors = {
-      username: "Username cannot be null",
-    };
-  }
-  next();
-};
-
-const validateEmail = (req: Request, res: Response, next: NextFunction) => {
-  const user = { ...req.body };
-  if (user.email === null) {
-    // @ts-expect-error 一旦型付けを無視する
-    req.validationErrors = {
-      // @ts-expect-error 一旦型付けを無視する
-      ...req.validationErrors,
-      email: "Email cannot be null",
-    };
-  }
-
-  next();
-};
-
 router.post(
   "/api/1.0/users",
-  validateUsername,
-  validateEmail,
+  check("username").notEmpty().withMessage("Username cannot be null"),
+  check("email").notEmpty().withMessage("Email cannot be null"),
   async (req: CustomRequest<CreateUserDTO>, res) => {
-    if (req.validationErrors) {
-      const response = { validationErrors: { ...req.validationErrors } };
-      return res.status(400).send(response);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = errors
+        .array()
+        .reduce((acc, error) => ({ ...acc, [error.param]: error.msg }), {});
+      return res.status(400).send({ validationErrors });
     }
 
     const user = { ...req.body };
