0f2691e32d18006479428ed309252736f29467ac
shimopino <shimoke4869@gmail.com>
2022-06-01 07:36:30 +0900

    Green: 日本語エラーメッセージを追加

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,28 @@
 import express from "express";
+import i18next from "i18next";
+import backend from "i18next-fs-backend";
+import middleware from "i18next-http-middleware";
 import UserRouter from "./user/UserRouter";
 
+i18next
+  .use(backend)
+  .use(middleware.LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    lng: "en",
+    ns: ["translation"],
+    defaultNS: "translation",
+    backend: {
+      loadPath: "./locales/{{lng}}/{{ns}}.json",
+    },
+    detection: {
+      lookupHeader: "accept-language",
+    },
+  });
+
 const app = express();
+
+app.use(middleware.handle(i18next));
 
 app.use(express.json());
 
--- backend/src/user/UserRouter.ts
+++ backend/src/user/UserRouter.ts
@@ -1,9 +1,7 @@
 import express from "express";
 import { CustomRequest, CreateUserDTO } from "./dtos";
 import { check, validationResult } from "express-validator";
-
 import { findByEmail, save } from "./UserService";
-import { User } from "./user";
 
 const router = express.Router();
 
@@ -11,39 +9,40 @@ router.post(
   "/api/1.0/users",
   check("username")
     .notEmpty()
-    .withMessage("Username cannot be null")
+    .withMessage("username_null")
     .bail()
     .isLength({ min: 4, max: 32 })
-    .withMessage("Must have min 4 and max 32 characters"),
+    .withMessage("username_size"),
   check("email")
     .notEmpty()
-    .withMessage("Email cannot be null")
+    .withMessage("email_null")
     .bail()
     .isEmail()
-    .withMessage("Email is not valid")
+    .withMessage("email_invalid")
     .custom(async (email) => {
       const user = await findByEmail(email);
       if (user) {
-        throw new Error("Email in use");
+        throw new Error("email_in_use");
       }
     }),
   check("password")
     .notEmpty()
-    .withMessage("Password cannot be null")
+    .withMessage("password_null")
     .bail()
     .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters")
+    .withMessage("password_size")
     .bail()
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
-    .withMessage(
-      "Password must be at least 1 uppercase, 1 lowercase letter and 1 number"
-    ),
+    .withMessage("password_pattern"),
   async (req: CustomRequest<CreateUserDTO>, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const validationErrors = errors
         .array()
-        .reduce((acc, error) => ({ ...acc, [error.param]: error.msg }), {});
+        .reduce(
+          (acc, error) => ({ ...acc, [error.param]: req.t(error.msg) }),
+          {}
+        );
       return res.status(400).send({ validationErrors });
     }
 
