762bdb00cfda69cc079cb322d67ac91f8ae3b713
shimopino <shimoke4869@gmail.com>
2022-06-02 08:19:27 +0900

    Green: メール送信用ロジックを追加

--- backend/src/user/UserService.ts
+++ backend/src/user/UserService.ts
@@ -2,6 +2,9 @@ import { hash } from "bcrypt";
 import { randomBytes } from "crypto";
 import { CreateUserDTO } from "./dtos";
 import { User } from "./user";
+import nodeMailer from "nodemailer";
+// @ts-expect-error 公式の型情報がないので一旦無視する
+import { stubTransport } from "nodemailer-stub";
 
 const generateToken = (length: number) => {
   return randomBytes(length).toString("hex").substring(0, length);
@@ -20,6 +23,14 @@ const save = async (body: CreateUserDTO) => {
     activationToken: generateToken(16),
   };
   await User.create(user);
+
+  const transporter = nodeMailer.createTransport(stubTransport);
+  await transporter.sendMail({
+    from: "My App <info@my-app.com>",
+    to: email,
+    subject: "Account Activation",
+    html: `Token is ${user.activationToken}`,
+  });
 };
 
 const findByEmail = async (email: string) => {
